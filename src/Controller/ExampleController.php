ecdee3c85308f92a273e8ad8614f30b899fb464a
funkylen <funkylen@gmail.com>
2024-03-07 19:47:39 +0300

    Make form

--- src/Controller/ExampleController.php
+++ src/Controller/ExampleController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use App\Entity\Example;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -23,16 +26,26 @@ class ExampleController extends AbstractController
         return $this->json($data);
     }
 
-
-    #[Route('/example/create')]
-    public function create(): Response
+    #[Route('/example/create', name: "app_example_create")]
+    public function create(Request $request): Response
     {
         $example = new Example();
-        $example->setName('created_at_' . time());
 
-        $this->em->persist($example);
-        $this->em->flush();
+        $form = $this->createFormBuilder($example)
+            ->add('name', TextType::class, ['label' => 'ExampleName'])
+            ->add('save', SubmitType::class, ['label' => 'Create Example'])
+            ->getForm();
 
-        return $this->json($example);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $example = $form->getData();
+
+            $this->em->persist($example);
+            $this->em->flush();
+
+            return $this->redirectToRoute('app_example_create');
+        }
+
+        return $this->render('example/create.html.twig', compact('form'));
     }
 }
